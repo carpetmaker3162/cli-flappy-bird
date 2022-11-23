ee15bffab67047fea2bebf932944372d3753ed80
can pooper <domesticatedhuman9999@gmail.com>
2022-11-23 17:27:32 -0500

    stop using queue module because im not stupid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # lmfao i just realized how sus my imports look
 from getch import getch
 import time
-import queue
 import threading
 import sys
 import termios
@@ -9,15 +8,20 @@ import os
 
 fd = sys.stdin.fileno()
 old_settings = termios.tcgetattr(fd)
-event_queue = queue.Queue()
+event_queue = []
 
 def process_keyboard_events(q):
     while True:
-        q.put(getch())
+        q.append(getch())
+
+def reset_terminal():
+    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
 thread = threading.Thread(target=process_keyboard_events, args=(event_queue,))
 thread.daemon = True
 thread.start()
+
+REFRESH_RATE = 0.25
 
 def index(char):
     if char is None or char == " " or len(char) == 0:
@@ -28,19 +32,21 @@ def index(char):
 last_update = time.time()
 
 while True:
-    if time.time() - last_update > 0.5:
+    if time.time() - last_update > REFRESH_RATE:
+        # refresh objects on the screen
         last_update = time.time()
     
-    if not event_queue.empty():
-        key = event_queue.get().strip()
+    if event_queue:
+        key = event_queue.pop()
         
+        # process keyboard events here
         if index(key) in (27, 3, 4):
-            #sys.stdout.flush()
+            sys.stdout.flush()
             break
         
         print(index(key), end=" ")
-        #sys.stdout.flush()
+        sys.stdout.flush()
 
 # reset terminal settings when you exit the game
 if os.name != "nt":
-    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    reset_terminal()
