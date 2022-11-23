b9ecc23b89760c1b2eca335b1213126eb6548d2b
can pooper <domesticatedhuman9999@gmail.com>
2022-11-22 20:28:06 -0500

    ...is this even a cleanup?

--- getch.py
+++ getch.py
@@ -1,34 +1,19 @@
-class _Getch:
-    def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
-            self.impl = _GetchUnix()
+def _unix_getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
-    def __call__(self):
-        return self.impl()
+def _win_getch():
+    return msvcrt.getch()
 
-class _GetchUnix:
-    def __init__(self):
-        import tty, sys
-
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-class _GetchWindows:
-    def __init__(self):
-        import msvcrt
-
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
-
-getch = _Getch()
+try:
+    import msvcrt
+    getch = _win_getch
+except ImportError:
+    import sys, tty, termios
+    getch = _unix_getch
